2d316d2bf27f6342f8c169bfb48a191faddfddab
Horia Coman <horia141@gmail.com>
2016-05-12 13:53:35 +0300

    Travis #16

--- bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pip install coveralls
-pip install coverage
-pip install --extra-index-url https://pypi.fury.io/$GEMFURY_KEY/silver-saas/ thriftplus
-pip install --extra-index-url "https://pypi.fury.io/$GEMFURY_KEY/silver-saas/" -r requirements.txt
